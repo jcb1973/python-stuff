308599990bfb4de18d10d1d473dde25aaf6310cd
John Cieslik-Bridgen <jcb@spotify.com>
2018-02-15 15:38:42 +0100

    Create air-quality-mail.py

---
+++ air-quality-mail.py
@@ -0,0 +1,107 @@
+import urllib.request
+import json
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+GMAIL_USER = "anon@anon.com"
+GMAIL_PASS = "topsecret"
+RECIPIENT = "myfriends@gmail.com"
+PM25_MAX = 25
+API_KEY = 'very-top-secret'
+
+req = urllib.request.Request('https://airapi.airly.eu/v1/mapPoint/measurements?latitude=49.97031&longitude=20.42504')
+req.add_header('Accept', 'application/json')
+req.add_header('apikey', API_KEY)
+
+response = urllib.request.urlopen(req)
+the_page = response.read()
+JSON_object = json.loads(the_page.decode('utf-8'))
+
+pm25_level =  (JSON_object["currentMeasurements"]["pm25"])
+
+
+if pm25_level <= 11:
+    level_str = "Low 1"
+    color = "#66FF99"
+elif 11 <= pm25_level <= 23:
+    level_str = "Low 2"
+    color = "#00FF00"
+elif 23 <= pm25_level <= 35:
+    level_str = "Low 3"
+    color = "#00cc00"
+elif 35 <= pm25_level <= 41:
+    level_str = "Moderate 1"
+    color = "#FFFF00"
+elif 41 <= pm25_level <= 47:
+    level_str = "Moderate 2"
+    color = "#FFCC00"
+elif 47 <= pm25_level <= 53:
+    level_str = "Moderate 3"
+    color = "#FF9900"
+elif 53 <= pm25_level <= 58:
+    level_str = "High 1"
+    color = "#FF3366"
+elif 58 <= pm25_level <= 64:
+    level_str = "High 2"
+    color = "#FF0000"
+elif 64 <= pm25_level <= 70:
+    level_str = "High 3"
+    color = "#990000"
+else:
+    level_str = "Very High"
+    color = "#CC00FF"
+
+percentage = pm25_level / PM25_MAX * 100
+percentage_str = '{0:.1f}'.format(percentage)
+subject = "Air quality level is currently " + percentage_str + "% of maximum level (PM 2.5)."
+print (subject)
+
+# me == my email address
+# you == recipient's email address
+me = GMAIL_USER
+you = RECIPIENT
+
+# Create message container - the correct MIME type is multipart/alternative.
+msg = MIMEMultipart('alternative')
+msg['Subject'] = "Air quality currently " + percentage_str + "% of norm"
+msg['From'] = me
+msg['To'] = you
+
+# Create the body of the message (a plain-text and an HTML version).
+text = subject
+html = """\
+<html>
+  <head></head>
+  <body>
+  <table border=”0″ cellpadding=”0″ cellspacing=”0″ width=”600″>
+  <td bgcolor="%s">
+    <p>Hi! This is John's automated mail to tell you the current air quality in Bochnia.<br>
+       %s<br>
+    </p>
+    </td>
+    </table>
+  </body>
+</html>
+""" % (color, subject)
+
+print (html)
+# Record the MIME types of both parts - text/plain and text/html.
+part1 = MIMEText(text, 'plain')
+part2 = MIMEText(html, 'html')
+
+# Attach parts into message container.
+# According to RFC 2046, the last part of a multipart message, in this case
+# the HTML message, is best and preferred.
+msg.attach(part1)
+msg.attach(part2)
+# Send the message via SMTP server.
+mail = smtplib.SMTP('smtp.gmail.com', 587)
+
+mail.ehlo()
+
+mail.starttls()
+
+mail.login(GMAIL_USER, GMAIL_PASS)
+mail.sendmail(me, you, msg.as_string())
+mail.quit()
