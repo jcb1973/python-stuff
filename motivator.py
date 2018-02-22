9a231b5dc5f21420ea6b6027ce0f6ebc63fab8e9
John Cieslik-Bridgen <john@estimote.com>
2018-02-22 08:41:45 +0100

    coffee

--- motivator.py
+++ motivator.py
@@ -3,7 +3,9 @@ import datetime
 from datetime import date 
 
 nosugar = date(2018,1,16)
+nocoffee = date(2018,2,21)
 
 today = datetime.date.today()
 
-print ("Now it's", abs(today - nosugar).days, "days without sugar")
+print ("Now it's", abs(today - nosugar).days, "days without sugar and", 
+    abs(today - nocoffee).days, "days without coffee")
