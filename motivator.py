fdb59b90dfe1f0906eeb9db0276ebf03127b8c00
JCB <jcb1973@gmail.com>
2018-04-18 09:08:08 +0200

    update

--- motivator.py
+++ motivator.py
@@ -2,10 +2,15 @@ import time
 import datetime
 from datetime import date 
 
-nosugar = date(2018,1,16)
-nocoffee = date(2018,2,21)
+nosugar = date(2018,4,18)
+noalcohol = date(2018,4,18)
 
 today = datetime.date.today()
+total_days = abs(today - nosugar).days
 
+print ("To: jcb1973@gmail.com")
+print ("Subject:", total_days, "days without sugar")
+print ("From: jcb1973@googlemail.com")
+print ("")
 print ("Now it's", abs(today - nosugar).days, "days without sugar and", 
-    abs(today - nocoffee).days, "days without coffee")
+    abs(today - noalcohol).days, "days without alcohol")
