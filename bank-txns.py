355fbb05e639542d563b4a566b465605b7802b74
John Cieslik-Bridgen <jcb1973@gmail.com>
2022-01-07 15:57:37 +0100

    Add files via upload

---
+++ bank-txns.py
@@ -0,0 +1,28 @@
+# get SEB bank transactions into suitable format for google sheets
+#txn-date, book-date, reference, desc, amount, balance		
+#2021­12­06 2021­12­04 5484753649 Postnord se /21­12­03 ­199,00 14.273,86 
+
+import sys
+import re
+
+#print(sys.stdout.encoding)
+
+txns = str(sys.argv[1])
+
+solution = re.compile(r"""
+	(\d{4}.\d{2}.\d{2}\s)
+	(\d{4}.\d{2}.\d{2}\s)
+	(\d*\s)
+	(.*\s)
+	(.*)
+	(\s.*)
+	(\s.*$)
+	""", re.VERBOSE)
+
+with open(txns, 'r', encoding='utf-8') as txnsFile:
+    
+	for row in txnsFile:
+		matches = (solution.findall(row))
+    	
+		for match in matches:
+			print(" ; ".join(match))
