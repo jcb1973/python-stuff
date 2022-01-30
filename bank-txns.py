de30faa58af500171228000df6dc5bb069925c48
John Cieslik-Bridgen <jbridgene@spotify.com>
2022-01-30 13:06:05 +0100

    added testing mode

--- bank-txns.py
+++ bank-txns.py
@@ -3,29 +3,39 @@
 import sys
 import re
 import pygsheets
+import random
 from pygsheets import custom_types
 from datetime import date
 
 headers = ["Bokföringsdatum", "Valutadatum", "Verifikationsnummer", "Text/mottagare ", "Belopp", "Saldo", "Invoice number", "VAT", "Notes"]		
 
+# what file are we looking for, are we in testing mode
+txns = str(sys.argv[1])
+testing_mode = str(sys.argv[2])
+
 with open('config.txt') as f:
     conf = dict([line.split() for line in f])
 
-sheet = (conf["SHEET"])
-gc = pygsheets.authorize()  
+if (testing_mode == "TEST"):
+	print ("testing mode only")
+	sheet = (conf["TESTING-SHEET"])
+else:
+	print ("live mode")
+	sheet = (conf["SHEET"])
+
+gc = pygsheets.authorize(client_secret='client_secret.json',)  
 sh = gc.open_by_key(sheet)
 
 # create a worksheet for the current month and switch to it
-today = date.today()
-newsheet = today.strftime("%B %Y")
-sh.add_worksheet(newsheet,rows=1, cols=9) 
-worksheet = sh.worksheet('title',newsheet)
+if (testing_mode == "TEST"):
+	new_sheet_name = date.today().strftime("%B %Y") + str(random.randint(1, 9999))
+else:
+	new_sheet_name = date.today().strftime("%B %Y")
+sh.add_worksheet(new_sheet_name,rows=1, cols=9) 
+worksheet = sh.worksheet('title',new_sheet_name)
 
 # set the titles in the first row
 worksheet.update_row(worksheet.rows, headers)
-
-# what file are we looking for
-txns = str(sys.argv[1])
 
 solution = re.compile(r"""
 	(\d{4}.\d{2}.\d{2}\s)
